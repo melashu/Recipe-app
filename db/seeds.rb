c78a5b70c7c8e06f84a7103c5bb990f588b7e80e
melashu <meshu102@outlook.com>
2023-03-07 22:09:52 +0300

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,7 @@
-Recipe.create!(name:'cooking', preparation_time: 1, cooking_time: 1,description:'show', user_id: User.last.id, public: true) ;
-Food.create!(name:'Apple',measurement_unit:'g',price: 15.5,quantity: 5, user_id: User.last.id)
+User.create!(name:'Nelashu', email: 'meshu.amare@gmail.com',password: '123456789')
+User.create!(name:'Mona', email: 'mona.amare@gmail.com',password: '123456789')
+
+Recipe.create!(name:'cooking', preparation_time: 1, cooking_time: 1,description:'show', user_id: User.first.id, public: true) ;
+
+Food.create!(name:'Apple',measurement_unit:'g',price: 15.5,quantity: 5, user_id: User.first.id)
+Food.create!(name:'Manago',measurement_unit:'g',price: 15.5,quantity: 5, user_id: User.first.id)
